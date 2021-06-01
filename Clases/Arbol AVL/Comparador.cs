bc1b8a904727d3c228feb570ce5a24088041a7a6
= <=>
2021-05-31 18:51:39 -0600

    Se agregan la carpeta CLASES las cueles contienen todos los elementos (clases) que se utilizaran en el proyecto final de programación III

---
+++ Clases/Arbol AVL/Comparador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace instagramforce.Clases.Arbol_AVL
+{
+    interface Comparador
+    {
+
+        bool igualQue(Object q);
+        bool menorQue(Object q);
+        bool menorIgualQue(Object q);
+        bool mayorQue(Object q);
+        bool mayorIgualQue(Object q);
+
+    }
+}
---
+++ Clases/Arbol AVL/Logical.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace instagramforce.Clases.Arbol_AVL
+{
+    class Logical
+    {
+
+        Boolean v;
+        public Logical(Boolean f)
+        {
+            v = f;
+        }
+        public void setLogical(Boolean f)
+        {
+            v = f;
+        }
+        public Boolean booleanValue()
+        {
+            return v;
+        }
+
+    }
+}
