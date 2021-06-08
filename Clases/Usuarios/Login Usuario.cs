54ff47f3d678ffd1fdb1bcc25f4d888efa438c20
= <=>
2021-06-07 22:40:30 -0600

    Se agregan el login a el proyecto

---
+++ Clases/Usuarios/Login Usuario.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using instagramforce.Clases.Arbol_AVL;
+
+namespace instagramforce.Clases.Usuarios
+{
+    class Login_Usuario: Comparador
+    {
+        public string USERNAME { get; set; }
+        public string PASSWORD { get; set; }
+
+        public Login_Usuario() { 
+        }
+        public Login_Usuario(string nickNameUser, string passowordUser)
+        {
+            this.USERNAME = nickNameUser;
+            this.PASSWORD = passowordUser;
+        }
+
+        public bool igualQue(object q)
+        {
+            Login_Usuario Paux = (Login_Usuario)q;
+            if (USERNAME.CompareTo(Paux.USERNAME) == 0)
+            {              
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool menorQue(object q)
+        {
+            Login_Usuario Paux = (Login_Usuario)q;
+            if (USERNAME.CompareTo(Paux.USERNAME) < 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool menorIgualQue(object q)
+        {
+            Login_Usuario Paux = (Login_Usuario)q;
+            if (USERNAME.CompareTo(Paux.USERNAME) <= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool mayorQue(object q)
+        {
+            Login_Usuario Paux = (Login_Usuario)q;
+            if (USERNAME.CompareTo(Paux.USERNAME) > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool mayorIgualQue(object q)
+        {
+            Login_Usuario Paux = (Login_Usuario)q;
+            if (USERNAME.CompareTo(Paux.USERNAME) >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- login.cs
+++ login.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
+using System.Xml;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using instagramforce.Clases.XML;
+using instagramforce.Clases.Arbol_AVL;
+using instagramforce.Clases.Usuarios;
 
 namespace instagramforce
 {
@@ -14,11 +19,42 @@ namespace instagramforce
     {
         string user = "";
         string password = "";
+        Xml_acciones xml_acciones = new Xml_acciones();
+        ArbolAVL loginUsuarios = new ArbolAVL();
+
         public login()
         {
             InitializeComponent();
+            string RunningPath = AppDomain.CurrentDomain.BaseDirectory;
+            string FileName = string.Format("{0}Resources\\userData.xml", Path.GetFullPath(Path.Combine(RunningPath, @"..\..\")));
+
+            if (File.Exists(FileName))
+            {
+                XmlDocument xmldoc = new XmlDocument();
+                xmldoc.Load(FileName);
+                XmlNodeList itemsNodes = xmldoc.SelectNodes("//Usuarios//Usuario");
+                foreach (XmlNode ItemNode in itemsNodes)
+                {
+                    string USERNAME = string.Empty, FULLNAME = string.Empty, PASSWORD = string.Empty, PROFILEIMAGE = string.Empty, BIRTHDATE = string.Empty;
+
+                    foreach (XmlNode item in ItemNode.SelectSingleNode("USERNAME"))
+                    {
+                        USERNAME = item.InnerText;
+                    }
+                    foreach (XmlNode item in ItemNode.SelectSingleNode("PASSWORD"))
+                    {
+                        PASSWORD = item.InnerText;
+                    }
+                    Login_Usuario loginUsers = new Login_Usuario(USERNAME, PASSWORD);
+                    loginUsuarios.insertar(loginUsers);
+                }
+            }
+            else
+            {
+                xml_acciones.crearXml(FileName, "Usuarios");
+            }
         }
-            
+
         private void login_Load(object sender, EventArgs e)
         {
             txtUser.Text = "Usuario";
@@ -109,12 +145,21 @@ namespace instagramforce
 
         private void gunaButton1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("user "+ user + " Contraseña "+ password);
+            Login_Usuario Objestudiantes = new Login_Usuario(txtUser.Text, txtPass.Text);
+            Login_Usuario ObjestudiantesEncontrado = (Login_Usuario)loginUsuarios.buscar(Objestudiantes).valorNodo();
 
-            instagramHome feedApp = new instagramHome();
-            this.Hide();
-            feedApp.Visible = true;
-            
+            if (ObjestudiantesEncontrado.PASSWORD == Objestudiantes.PASSWORD && ObjestudiantesEncontrado.USERNAME == Objestudiantes.USERNAME)
+            {
+                instagramHome feedApp = new instagramHome();
+                this.Hide();
+                feedApp.Visible = true;
+            }
+            else
+            {
+                MessageBox.Show("El usuario o la contraseña son incorrectas\n"+"¡Vuelve a interntarlo!");
+                txtPass.Text = "";
+                txtUser.Text = "";
+            }
         }
 
         private void login_Click(object sender, EventArgs e)
